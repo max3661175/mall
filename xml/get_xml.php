4ff1260e874754122dfebf3c3aaf3fe73a8cbb5a
max3661175 <xuhang3661175@vip.qq.com>
2016-04-21 10:57:08 +0800

    Added files via upload

---
+++ xml/get_xml.php
@@ -0,0 +1,56 @@
+<?php
+
+class AminoAcid {
+    var $Num;  //key 
+    var $ordercode;    
+    var $code;  
+    var $mobile;   
+
+    function AminoAcid ($aa)
+    {
+        foreach ($aa as $k=>$v)
+            $this->$k = $aa[$k];
+    }
+}
+
+function readDatabase($filename)
+{
+    // 读取 aminoacids 的 XML 数据
+    $data = implode("",file($filename));
+    $parser = xml_parser_create();
+    xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
+    xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);
+    xml_parse_into_struct($parser, $data, $values, $tags);
+    xml_parser_free($parser);
+
+    // 遍历 XML 结构
+    foreach ($tags as $key=>$val) {
+        if ($key == "molecule") {
+            $molranges = $val;
+            // each contiguous pair of array entries are the
+            // lower and upper range for each molecule definition
+            for ($i=0; $i < count($molranges); $i+=2) {
+                $offset = $molranges[$i] + 1;
+                $len = $molranges[$i + 1] - $offset;
+                $tdb[] = parseMol(array_slice($values, $offset, $len));
+            }
+        } else {
+            continue;
+        }
+    }
+    return $tdb;
+}
+
+function parseMol($mvalues)
+{
+    for ($i=0; $i < count($mvalues); $i++) {
+        $mol[$mvalues[$i]["tag"]] = $mvalues[$i]["value"];
+    }
+    return new AminoAcid($mol);
+}
+
+$db = readDatabase("moldb.xml");
+
+print_r($db);//得到一个数组
+
+?>
